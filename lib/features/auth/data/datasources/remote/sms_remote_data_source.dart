771ff2b1dd1aaea3b474e76adf062c4f2f834cd8
Joldubaev <kylychbekjoldubaev@gmail.com>
2024-07-17 19:55:02 +0600

    sms otp

--- lib/features/auth/data/datasources/remote/sms_remote_data_source.dart
+++ lib/features/auth/data/datasources/remote/sms_remote_data_source.dart
@@ -1,12 +1,10 @@
 import 'dart:convert';
 import 'dart:developer';
-import 'package:diyar/shared/utils/utils.dart';
 import 'package:xml/xml.dart' as xml;
 import 'package:dio/dio.dart';
 
 abstract class SmsRemoteDataSource {
   Future<void> sendSms(String code, String phone);
-  Future<bool> checkPhoneNumberExists(String phone);
 }
 
 class SmsRemoteDataSourceImpl implements SmsRemoteDataSource {
@@ -16,13 +14,6 @@ class SmsRemoteDataSourceImpl implements SmsRemoteDataSource {
 
   @override
   Future<void> sendSms(String code, String phone) async {
-    // Check if the phone number already exists
-    final phoneExists = await checkPhoneNumberExists(phone);
-    if (phoneExists) {
-      showToast('Такой номер телефона уже зарегистрирован');
-      throw Exception('Такой номер телефона уже зарегистрирован');
-    }
-
     final builder = xml.XmlBuilder();
     builder.processing('xml', 'version="1.0" encoding="UTF-8"');
     builder.element('message', nest: () {
@@ -60,26 +51,6 @@ class SmsRemoteDataSourceImpl implements SmsRemoteDataSource {
     } catch (e) {
       log('Ошибка: $e');
       throw Exception('Ошибка при отправке SMS: $e');
-    }
-  }
-
-  @override
-  Future<bool> checkPhoneNumberExists(String phone) async {
-    try {
-      final response = await dio.get(
-        'https://your-api-endpoint/check_phone', // Replace with your actual endpoint
-        queryParameters: {'phone': phone},
-      );
-
-      if (response.statusCode == 200) {
-        final data = response.data as Map<String, dynamic>;
-        return data['exists'] as bool;
-      } else {
-        throw Exception('Ошибка: ${response.statusCode}');
-      }
-    } catch (e) {
-      log('Ошибка при проверке номера телефона: $e');
-      throw Exception('Ошибка при проверке номера телефона: $e');
     }
   }
 }
